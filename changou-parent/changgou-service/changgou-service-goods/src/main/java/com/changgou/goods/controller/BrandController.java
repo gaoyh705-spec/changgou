bf7b76ee73b649f1db6b3d1e8f741b2e69f37a30
gaoyh705-spec <gao596541840@qq.com>
2021-03-01 01:07:02 +0800

    first commit

---
+++ changou-parent/changgou-service/changgou-service-goods/src/main/java/com/changgou/goods/controller/BrandController.java
@@ -0,0 +1,32 @@
+package com.changgou.goods.controller;
+
+import com.changgou.goods.pojo.Brand;
+import com.changgou.goods.service.BrandService;
+import entity.Result;
+import entity.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/brand")
+@CrossOrigin
+public class BrandController {
+
+    @Autowired
+    private BrandService brandService;
+
+    /***
+     * 查询全部数据
+     * @return
+     */
+    @GetMapping
+    public Result<Brand> findAll(){
+        List<Brand> brandList = brandService.findAll();
+        return new Result<Brand>(true, StatusCode.OK,"查询成功",brandList) ;
+    }
+}
